1ed26902e41b59f1a23ba7f0211bedb72262fc5a
Gautam Gupta <gautamgupta986047@gmail.com>
2025-03-31 22:28:40 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";  // Default XAMPP username
-$password = "";  // Default XAMPP password is empty
+$servername = "recipe-1.infinityfreeapp.com";
+$username = "if0_38644444";  // Default XAMPP username
+$password = "gautam121212";  // Default XAMPP password is empty
 $dbname = "getsetrecipe";  // Make sure this database exists!
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($recipe-1.infinityfreeapp.com, $if0_38644444, $gautam121212, $getsetrecipe);
 
 // Check connection
 if ($conn->connect_error) {
